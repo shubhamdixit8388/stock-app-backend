36ed1d8d9f01919b0ad03f65234ee1909f24bc16
Shubham <svdixit8388@gmail.com>
2021-10-17 16:29:29 +0530

    get indices model added for bse and saving data in DB

--- controllers/bse-controller.js
+++ controllers/bse-controller.js
@@ -1,13 +1,21 @@
 const stockExchangeService = require("../services/stock-exchange-service");
+const GetIndicesBSE = require("../models/get-indices-bse-model");
 
 exports.getIndices = (req, res) => {
   stockExchangeService
     .getIndices("https://api.bseindia.com/bseindia/api/Indexmasternew/GetData")
-    .then((result) => {
+    .then(async (result) => {
+      let getIndicesBSE;
+      if (result && result.data.data && result.data.data.length) {
+        await GetIndicesBSE.findOneAndDelete();
+        getIndicesBSE = await GetIndicesBSE.create(result.data);
+      } else {
+        getIndicesBSE = await GetIndicesBSE.findOne();
+      }
       res.status(200).send({
         status: 200,
         message: "Success",
-        data: result.data,
+        data: getIndicesBSE,
       });
     })
     .catch((error) => {
