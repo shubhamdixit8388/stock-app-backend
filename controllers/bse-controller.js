52406ed265d11f407b78483d49df2c3a0fcca40b
Shubham <svdixit8388@gmail.com>
2021-10-21 22:11:50 +0530

    get indices for bse

--- controllers/bse-controller.js
+++ controllers/bse-controller.js
@@ -1,27 +1,45 @@
 const stockExchangeService = require("../services/stock-exchange-service");
-const GetIndicesBSE = require("../models/get-indices-bse-model");
+const StockExchange = require("../models/stock-exchange-model");
 
 exports.getIndices = (req, res) => {
   stockExchangeService
     .getIndices("https://api.bseindia.com/bseindia/api/Indexmasternew/GetData")
     .then(async (result) => {
-      let getIndicesBSE;
       if (result && result.data.data && result.data.data.length) {
-        await GetIndicesBSE.findOneAndDelete();
-        getIndicesBSE = await GetIndicesBSE.create(result.data);
+        let query = {_id: '616c2d4c3250fc3150da01b0'};
+        const data = {$set: {'bse.allIndices.data': result.data.data}}
+        StockExchange.findOneAndUpdate(query, data, {new: true}, (err, doc) => {
+              if(err) {
+                res.status(500).send({
+                  status: "500",
+                  message: err.message,
+                });
+              } else {
+                res.status(200).send({
+                  status: 200,
+                  message: "Success",
+                  stockType: "BSE",
+                  data: doc.bse.allIndices.data,
+                });
+              }
+            });
       } else {
-        getIndicesBSE = await GetIndicesBSE.findOne();
+        const data = await StockExchange.findOne({}, {'bse.allIndices.data': 1});
+        res.status(200).send({
+          status: 200,
+          message: "Success",
+          stockType: "BSE",
+          data: data.bse.allIndices.data,
+        });
       }
+    })
+    .catch(async () => {
+      const data = await StockExchange.findOne({}, {'bse.allIndices.data': 1});
       res.status(200).send({
         status: 200,
         message: "Success",
-        data: getIndicesBSE,
-      });
-    })
-    .catch((error) => {
-      res.status(500).send({
-        status: "500",
-        message: error.message,
+        stockType: "BSE",
+        data: data.bse.allIndices.data,
       });
     });
 };
---
+++ models/stock-exchange-model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const stockExchange = new mongoose.Schema({
+  bse: {
+    allIndices: new mongoose.Schema({
+      data: Object
+    }, { timestamps: true })
+  },
+  nse: {
+    allIndices: new mongoose.Schema({
+      data: Object
+    }, { timestamps: true })
+  }
+})
+
+module.exports = mongoose.model('StockExchange', stockExchange);
