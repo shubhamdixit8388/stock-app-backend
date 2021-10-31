4149e116f9423b44b4c2a1630c1d08a5adf15b27
Shubham <svdixit8388@gmail.com>
2021-10-31 15:30:31 +0530

    fetchinf all user tokens who has field notificationEnabled = false

--- services/user-service.js
+++ services/user-service.js
@@ -1,6 +1,6 @@
 const User = require('../models/user-model');
 
 exports.getUsersIdWithExpoToken = async () => {
-  const users = await User.find({}, 'token');
+  const users = await User.find({notificationDisabled: false}, 'token');
   return users;
 }
